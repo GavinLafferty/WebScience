5553408681fd0b44580d1b8961324326697848fe
douglasrussell86 <2386206r@student.gla.ac.uk>
2021-03-02 15:57:53 +0000

    REST API attempt 1

--- restCrawler.py
+++ restCrawler.py
@@ -8,20 +8,24 @@ import sys
 import emoji
 import re
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+
 #sets up MongoDB host
 MONGO_HOST = 'mongodb://localhost/twitterdb'
 
 
 #  please put your credentials below - very important
-consumer_key = "Q8JJKByeNa2uWAocFcSHwsI2v"
-consumer_secret ="3eLkjKsIltXhP1dxRCJLtvYJLULrw0JdBAgUpy8t1DT2X9tHZM"
-access_token ="2506970325-NnZvsAsTdYrcoGtSx7YEldFAc4u1R8cQat3MZnq"
-access_token_secret ="Vhmm5rAtuFq53xSD6lBItgW4zQRdD1jW1CfexLgyDXXRq"
+consumer_key = "Y7Yv9GWbovGLcVfXqui9GRqkC"
+consumer_secret ="3bgadhXK5UX81liQdMb7mwVeBgAy9A2xgFTNT8ViIi3TeQOVXZ"
+access_token ="901441890670829568-jMxcpuH2vLuNtE0PQGBq5cse1tcIpqx"
+access_token_secret ="8v3ymcUyE648hm1ucyUD9aRjcyCtwLDUPMz4rlZvIYevZ"
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret )
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth,parser=tweepy.parsers.JSONParser())
 if (not api):
     print('Can\'t authenticate')
     print('failed cosumeer id ----------: ', consumer_key )
@@ -33,7 +37,7 @@ client = MongoClient(MONGO_HOST) #is assigned local port
 dbName = "twitterdb" # set-up a MongoDatabase
 db = client.twitterdb
 collName = 'tweets' # here we create a collection
-collection = db[collName] #  This is for the Collection  put in the DB
+tweet_collection = db[collName] #  This is for the Collection  put in the DB
 
 
 def strip_emoji(text):
@@ -62,18 +66,16 @@ def processTweets(tweet):
     qtweet=False
     retweet=False
 
-    # print(t)
-
     # Pull important data from the tweet to store in the database.
     try:
         created = tweet['created_at']
         tweet_id = tweet['id_str']  # The Tweet ID from Twitter in string format
         username = tweet['user']['screen_name']  # The username of the Tweet author
         # followers = t['user']['followers_count']  # The number of followers the Tweet author has
-        text = tweet['text']  # The entire body of the Tweet
+        text = tweet['full_text']  # The entire body of the Tweet
     except Exception as e:
         # if this happens, there is something wrong with JSON, so ignore this tweet
-        print(e)
+        print("bad tweet")
         return None
 
     try:
@@ -155,38 +157,52 @@ def processTweets(tweet):
     tweet1 = {'_id' : tweet_id, 'date': created, 'username': username,  'text' : text,  'geoenabled' : geoenabled,  'coordinates' : coordinates,  'location' : location,  'place_name' : place_name, 'place_country' : place_country, 'country_code': place_countrycode,  'place_coordinates' : place_coordinates, 'hashtags' : hList, 'mentions' : mList, 'source' : source, 'retweet':retweet, 'quote_tweet':qtweet}
     return tweet1
 
+def getText(tweet_collection):
+    usernames,hashtags,text = [],[],[]
+    alldata = tweet_collection.find()
+    for tweet in alldata:
 
+        ulower = str(tweet['username']).lower()
+        htlower = str(tweet['hashtags']).lower()
+        tlower = str(tweet['text']).lower()
+        usernames.append(ulower)
+        hashtags.append(htlower)
+        text.append(tlower)
 
-class StreamListener(tweepy.StreamListener):
-  #This is a class provided by tweepy to access the Twitter Streaming API.
+    return usernames,hashtags,text
+    
+def cluster(usernames,hashtags,texts):
+    vectorizer = TfidfVectorizer(stop_words='english')
+    n = 12
 
-    global geoEnabled
-    global geoDisabled
-    def on_connect(self):
-        # Called initially to connect to the Streaming API
-        print("You are now connected to the streaming API.")
+    username_vector = vectorizer.fit_transform(usernames)
+    hashtag_vector = vectorizer.fit_transform(hashtags)
+    text_vector = vectorizer.fit_transform(texts)
 
+    usernames_cluster = KMeans(n_clusters=n,init='k-means++',max_iter=100,n_init=1)
+    usernames_cluster.fit(username_vector)
 
+    hashtags_cluster = KMeans(n_clusters=n,init='k-means++',max_iter=100,n_init=1)
+    hashtags_cluster.fit(hashtag_vector)
 
-    def on_error(self, status_code):
-        # On error - if an error occurs, display the error / status code
-        print('An Error has occured: ' + repr(status_code))
-        return False
+    text_cluster = KMeans(n_clusters=n,init='k-means++',max_iter=100,n_init=1)
+    text_cluster.fit(text_vector)
 
-    def on_data(self, data):
-        #This is where each tweet is collected
-        # let us load the  json data
-        t = json.loads(data)
-        #  now let us process the wteet so that we will deal with cleaned and extracted JSON
-        tweet = processTweets(t)
-        # print(tweet)
-        # now insert it
-        #  for this to work you need to start a local mongodb server
-        try:
-             collection.insert_one(tweet)
-        except Exception as e:
-             print(e)
-            # this means some Mongo db insertion errort
+    return vectorizer,usernames_cluster,hashtags_cluster,text_cluster
+
+def clusterize(vectorizer,group):
+    n = 12
+    centroids = group.cluster_centers_.argsort()[:,::-1]
+    text = vectorizer.get_feature_names()
+    clusters = []
+    for centre in range(n):
+        print ("CLUSTER %d:" % centre)
+        cluster = []
+        for i in centroids[centre, :5]:
+            print (' %s' % text[i])
+            cluster.append(text[i])
+        clusters.append(cluster)
+    return clusters
 
 
 
@@ -195,46 +211,65 @@ class StreamListener(tweepy.StreamListener):
 
 # WORDS = ['manhattan' , 'new york city', 'statue of liberty']
 # LOCATIONS = [ -75,40,-72,42] # new york city
-Loc_UK = [-10.392627, 49.681847, 1.055039, 61.122019] # UK and Ireland
-Words_UK =["Boris", "Prime Minister", "Tories", "UK", "London", "England", "Manchester", "Sheffield", "York", "Southampton", \
- "Wales", "Cardiff", "Swansea" ,"Banff", "Bristol", "Oxford", "Birmingham" ,"Scotland", "Glasgow", "Edinburgh", "Dundee", "Aberdeen", "Highlands" \
-"Inverness", "Perth", "St Andrews", "Dumfries", "Ayr" \
-"Ireland", "Dublin", "Cork", "Limerick", "Galway", "Belfast"," Derry", "Armagh" \
-"BoJo", "Labour", "Liberal Democrats", "SNP", "Conservatives", "First Minister", "Surgeon", "Chancelor" \
-"Boris Johnson", "BoJo", "Keith Stramer"]
+#Loc_UK = [-10.392627, 49.681847, 1.055039, 61.122019] # UK and Ireland
+# Words_UK =["Boris", "Prime Minister", "Tories", "UK", "London", "England", "Manchester", "Sheffield", "York", "Southampton", \
+#  "Wales", "Cardiff", "Swansea" ,"Banff", "Bristol", "Oxford", "Birmingham" ,"Scotland", "Glasgow", "Edinburgh", "Dundee", "Aberdeen", "Highlands" \
+# "Inverness", "Perth", "St Andrews", "Dumfries", "Ayr" \
+# "Ireland", "Dublin", "Cork", "Limerick", "Galway", "Belfast"," Derry", "Armagh" \
+# "BoJo", "Labour", "Liberal Democrats", "SNP", "Conservatives", "First Minister", "Surgeon", "Chancelor" \
+# "Boris Johnson", "BoJo", "Keith Stramer"]
 
-print("Tracking: " + str(Words_UK))
+# print("Tracking: " + str(Words_UK))
 #  here we ste the listener object
-listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
-streamer = tweepy.Stream(auth=auth, listener=listener)
-streamer.filter(locations= Loc_UK, track = Words_UK, languages = ['en'], is_async=True) #locations= Loc_UK, track = Words_UK,
+# listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
+# streamer = tweepy.Stream(auth=auth, listener=listener)
+# streamer.filter(locations= Loc_UK, track = Words_UK, languages = ['en'], is_async=True) #locations= Loc_UK, track = Words_UK,
 #  the following line is for pure 1% sample
 # we can only use filter or sample - not both together
 # streamer.sample(languages = ['en'])
 
-Place =  'London'
+# Place =  'London'
 Lat   =  '51.450798'
 Long  =  '-0.137842'
 geoTerm=Lat+','+Long+','+'10km'
 #
 
 last_id =  None
-counter =0
-sinceID = None
+# counter =0
+# sinceID = None
 
-results = True
+# results = True
 
-while results:
-    # print(geoTerm)
+# while results:
+#     # print(geoTerm)
 
-    if (counter < 180 ):
-        try:
-            results = api.search(geocode=geoTerm, count=100, lang="en", tweet_mode='extended', max_id=last_id) #, since_id = sinceID)
-            print(results)
-        except Exception as e:
-            print(e)
-        counter += 1
-    else:
-        #  the following let the crawler to sleep for 15 minutes; to meet the Tiwtter 15 minute restriction
-        time.sleep(15*60)
+#     if (counter < 180 ):
+#         try:
+#             results = api.search(geocode=geoTerm, count=100, lang="en", tweet_mode='extended', max_id=last_id) #, since_id = sinceID)
+#             print(results)
+#         except Exception as e:
+#             print(e)
+#         counter += 1
+#     else:
+#         #  the following let the crawler to sleep for 15 minutes; to meet the Tiwtter 15 minute restriction
+#         time.sleep(15*60)
 #
+sc,ht,t = getText(tweet_collection)
+vec,scK,htK,tK = cluster(sc,ht,t)
+
+clusters = clusterize(vec,tK)
+
+for cluster in clusters:
+    name = ""
+    for word in cluster:
+        name = name + " " + word
+
+    collection = db[name]
+    for word in cluster:
+        results = api.search(q=word,geocode=geoTerm, count=10, lang="en", tweet_mode='extended', max_id=last_id) #, since_id = sinceID)
+        for tweet in results["statuses"]:
+            try:
+                collection.insert_one(processTweets(tweet))
+            except Exception as e:
+                print(e)
+
